bdb791a69cb442c516e1e168a8bbb069a9c9653d
leson207 <sonthaile2002@gmail.com>
2026-01-02 10:54:58 +0700

    many thing

--- scripts/tmp.sh
+++ scripts/tmp.sh
@@ -1,7 +1,18 @@
 # Execute command
 # Undo command
+# idempotent
+
 # ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 # hwclock --systohc
+
+# LOCALE="en_US.UTF-8"
+# KEYMAP="de-latin1"
+# sed -i "s/# $LOCALE/$LOCALE/" /etc/locale.gen
+# locale-gen
+# echo "LANG=$LOCALE" > /etc/locale.conf
+# echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+
+# echo "victor" > /etc/hostname
 
 # - name: Disable root SSH login
 #   ansible.builtin.lineinfile:
@@ -20,3 +31,53 @@
 # Current=sddm-astronaut-theme" | sudo tee /etc/sddm.conf
 # echo "[General]
 # InputMethod=qtvirtualkeyboard" | sudo tee /etc/sddm.conf.d/virtualkbd.conf
+
+# sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
+# sudo sed -i 's/^#ILoveCandy/ILoveCandy/' /etc/pacman.conf
+# sudo sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
+# sudo sed -i 's/^#DownloadUser = alpm/DownloadUser = alpm/' /etc/pacman.conf
+# VerbosePkgLists
+
+# systemctl --user unset-environment GTK_IM_MODULE
+# systemctl --user show-environment | grep GTK_IM_MODULE
+
+# sudo systemctl disable systemd-timesyncd
+# sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
+# sudo journalctl --vacuum-time=7d
+
+# /etc/sysctl.d/99-desktop.conf
+# vm.swappiness=10
+# kernel.sched_autogroup_enabled=1
+# fs.inotify.max_user_watches=524288
+
+
+# sudo ln -s /usr/lib/systemd/system/cpupower.service /etc/systemd/system/multi-user.target.wants/
+# sudo nano /etc/default/cpupower
+# governor="performance" or "schedutil"
+
+sudo pacman -Rns $(pacman -Qtdq)
+
+~/.config/systemd/user/idle-cleanup.service
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/pkill -f chromium
+systemctl --user enable idle-cleanup.timer
+
+
+sudo nano /etc/systemd/logind.conf
+HandleLidSwitch=suspend
+HandleLidSwitchDocked=ignore
+
+
+sudo pacman -S tlp
+sudo systemctl enable tlp
+CPU_ENERGY_PERF_POLICY_ON_BAT=power
+
+
+
+systemd-analyze blame
+systemctl status <service>
+journalctl -xe
+
+/boot/loader/entries/arch.conf
+options mitigations=auto,nosmt
--- victor/.config/lazyvim/lua/config/keymaps.lua
+++ victor/.config/lazyvim/lua/config/keymaps.lua
@@ -8,34 +8,36 @@ vim.keymap.set("i", "<A-h>", "<Left>", { desc = "Cursor Left", silent = true })
 -- Alt+l moves cursor right in insert mode
 vim.keymap.set("i", "<A-l>", "<Right>", { desc = "Cursor Rightj", silent = true })
 
---   vim.fn.setreg('"', saved_reg, saved_regtype)  -- restore unnamed register
--- end, { noremap = true, silent = true, desc = 'Copy to clipboard without affecting registers' })
--- vim.keymap.set('v', '<leader>y', function()
---   local saved_reg = vim.fn.getreg('"')
---   local saved_regtype = vim.fn.getregtype('"')
---
---   vim.cmd('normal! "+y')  -- yank to system clipboard
---
---   vim.fn.setreg('"', saved_reg, saved_regtype)  -- restore with type
--- end, { noremap = true, silent = true, desc = 'Copy to clipboard without affecting registers' })
-
 vim.keymap.set('v', '<leader>y', function()
 	local old_unnamed = vim.fn.getreg('"')
 	vim.cmd('normal! "+y')
 	vim.fn.setreg('"', old_unnamed)
 end, { noremap = true, silent = true, desc = 'Copy to clipboard without affecting registers' })
---
--- vim.keymap.set({'n', 'i', 't'}, '<leader>p', function()
--- 	local old_unnamed = vim.fn.getreg('"')
---
--- 	local mode = vim.fn.mode()
--- 	if mode == 'n' or mode=='i' then
--- 		vim.cmd('normal! "+p')
--- 	elseif mode == 't' then
--- 		local clip = vim.fn.getreg('+')  -- system clipboard
--- 		vim.api.nvim_feedkeys(clip, 't', false)   -- sends text to terminal input
--- 	end
---
--- 	vim.fn.setreg('"', old_unnamed)  -- restore unnamed register
--- end, { noremap = true, silent = true, desc = 'Paste from system clipboard' })
 
+vim.keymap.set('n', '<leader>cy', function()
+	vim.diagnostic.open_float()
+	vim.diagnostic.open_float()
+	local win_id = vim.fn.win_getid()    -- get the window ID of the floating window
+	vim.cmd("normal! j")                 -- move down one row
+	vim.cmd("normal! VG")                -- select everything from that row down
+	vim.cmd("normal! \"+y")                 -- yank selected text
+	vim.api.nvim_win_close(win_id, true) -- close the floating window by its ID
+	print("Copy diagnostics info to clipboard")
+end, { noremap = true, silent = true, desc = "Copy diagnostics info to clipboard" })
+
+vim.keymap.set('t', '<leader>ty', function ()
+	local bufnr = vim.api.nvim_get_current_buf()
+	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+	local start_line = 1
+
+	-- adjust this pattern to your shell prompt
+	for i = #lines-4, 1, -1 do
+		if string.match(lines[i], " ~") then
+		start_line = i
+		break
+		end
+	end
+	local output = table.concat(vim.list_slice(lines, start_line, #lines-4), "\n")
+	vim.fn.setreg('+', output)
+	print("Copy last command output to clipboard")
+end, { noremap = true, silent = true, desc = "Copy last command output to clipboard"})
