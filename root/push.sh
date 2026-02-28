b8a07fce3326e8b09c54d4d1d5d888cf626638e1
leson207 <sonthaile2002@gmail.com>
2026-02-28 22:47:20 +0700

    delete old config form

--- root/push.sh
+++ root/push.sh
@@ -1,13 +1,33 @@
 push.sh --all | destinations path1, path2, path3, ...
 
+list of file=[
+
+]
+
 #!/usr/bin/env bash
 set -euo pipefail
 
-SRC="/"
-DEST="/home/victor/dotfiles/root"
+SRC="/home/victor/dotfiles/root"
+DEST="/"
+
+read -rp "Do you want to continue? [y/n]: " answer
+
+case "$answer" in
+[Yy])
+    echo "✅ Continuing..."
+    ;;
+[Nn] | "")
+    echo "❌ Cancelled."
+    exit 1
+    ;;
+*)
+    echo "⚠ Invalid input. Please enter y or n."
+    exit 1
+    ;;
+esac
 
 sudo rsync -rvh --relative \
     --no-perms --no-owner --no-group \
     --info=progress2 \
-    --files-from=sources.txt \
+    --files-from=destinations.txt \
     "$SRC" "$DEST"
